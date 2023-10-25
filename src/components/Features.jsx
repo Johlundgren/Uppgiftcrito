e670ff07c96c86633226327b97f00c82676c17fe
Johan <gunbald@live.se>
2023-10-25 16:45:13 +0200

    Features component

---
+++ src/components/Features.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Features = () => {
+  return (
+    <div className="features">
+    <div className="container">
+    <section className="feature-section flex-section">
+        <div className="section-title">
+            <p>Features</p>
+            <h2>Our Accounting is trusted by thousand of companies</h2>
+            <div className="center-content">
+                <a className="btn-yellow" href="features.html">Learn More<i className="fa-regular fa-arrow-up-right"></i></a>
+            </div>
+        </div>
+    </section>
+        <section className="feature-section grid-section">
+            <div className="feature">
+                <i className="fa-light fa-handshake"></i>
+                <h3>Business Advice</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit</p>
+            </div> 
+            <div className="feature">
+                <i className="fa-sharp fa-light fa-lightbulb-on"></i>
+                <h3>Startup Business</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit</p>
+            </div>
+            <div className="feature">
+                <i className="fa-sharp fa-light fa-chart-mixed-up-circle-dollar"></i>
+                <h3>Financial Advice</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit</p>
+            </div>
+            <div className="feature">
+                <i className="fa-light fa-cube"></i>
+                <h3>Risk Management</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit</p>
+            </div>
+        </section>
+    </div>
+    </div>
+  )
+}
+
+export default Features
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '@components/Header'
 import Showcase from '../components/Showcase'
 import BusinessLogo from '../components/BusinessLogo'
+import Features from '../components/Features'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
         <Header />
         <Showcase />
         <BusinessLogo />
+        <Features />
 
     </>
   )
