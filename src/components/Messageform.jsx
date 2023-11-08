21e8f29d0f4b02e1cdd776c40c803ad9ecc54381
Johan <gunbald@live.se>
2023-11-08 14:16:11 +0100

    alla g kritensier klara

--- src/components/Messageform.jsx
+++ src/components/Messageform.jsx
@@ -1,26 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react'
 import '@styles/_Messageform.scss'
 
 const Messageform = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  })
+
+  const { name, email, message } = formData;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const apiUrl = 'https://win23-assignment.azurewebsites.net/api/contactform'
+
+      const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      })
+
+      if (response.status === 200) {
+        console.log('Message Success!');
+      } else {
+        console.log('Oops, something went wrong with the message');
+      }
+    } catch (error) {
+      console.error('Something went wrong', error);
+    }
+  }
+
   return (
     <div className="form-message">
       <div className="container">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Leave us a message<br></br> for any information.</h1>
           <div className="input-group">
-            <input type="text" id="username" placeholder="Name" />
+            <input
+              type="text"
+              id="username"
+              name="name"
+              value={name}
+              placeholder="Name"
+              onChange={handleChange}
+            />
           </div>
           <div className="input-group">
-            <input type="email" id="email" placeholder="E-mail" />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="E-mail"
+              onChange={handleChange}
+            />
           </div>
           <div className="input-group">
-            <textarea id="message" placeholder="Your Message"></textarea>
+            <textarea
+              id="message"
+              name="message"
+              value={message}
+              placeholder="Your Message"
+              onChange={handleChange}
+            ></textarea>
           </div>
-          <button className="btn-yellow">Send Message <i className="fa-regular fa-arrow-up-right"></i></button>
+          <button type="submit" className="btn-yellow">Send Message <i className="fa-regular fa-arrow-up-right"></i></button>
         </form>
       </div>
     </div>
-  );
+  )
 }
 
-export default Messageform;
+export default Messageform
--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -4,6 +4,8 @@ import Letsconnect from '../components/Letsconnect'
 import Companyinformation from '../components/Companyinformation'
 import Messageform from '../components/Messageform'
 import Mappicture from '../components/Mappicture'
+import Footerlinks from '../components/Footerlinks'
+import Footer from '../components/Footer'
 
 const Contact = () => {
   return (
@@ -11,8 +13,10 @@ const Contact = () => {
     <Header />
     <Letsconnect />
     <Companyinformation />
-    <Messageform />
+    <Messageform /> 
     <Mappicture />
+    <Footerlinks />
+    <Footer />
     </>  
   )
 }
