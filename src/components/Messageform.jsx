f61afc0bb8a54b64d608f8ac1548d2ecd375b6bf
Johan <gunbald@live.se>
2023-11-08 14:33:19 +0100

    allting för g klart

--- src/components/Messageform.jsx
+++ src/components/Messageform.jsx
@@ -8,18 +8,26 @@ const Messageform = () => {
     message: ''
   })
 
-  const { name, email, message } = formData;
+  const [messageSent, setMessageSent] = useState(false)
+
+  const { name, email, message } = formData
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
+    const { name, value } = e.target
     setFormData({
       ...formData,
       [name]: value
     })
   }
 
+
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
+
+    if (!name || !email || !message) {
+      alert('Looks like you missed a field!');
+      return;
+    }
 
     try {
       const apiUrl = 'https://win23-assignment.azurewebsites.net/api/contactform'
@@ -33,51 +41,53 @@ const Messageform = () => {
       })
 
       if (response.status === 200) {
-        console.log('Message Success!');
+        console.log('Message Success!')
+        setMessageSent(true)
       } else {
-        console.log('Oops, something went wrong with the message');
+        console.log('Oops, something went wrong with the message')
       }
     } catch (error) {
-      console.error('Something went wrong', error);
+      console.error('Something went wrong', error)
     }
   }
 
   return (
     <div className="form-message">
       <div className="container">
-        <form onSubmit={handleSubmit}>
-          <h1>Leave us a message<br></br> for any information.</h1>
-          <div className="input-group">
-            <input
-              type="text"
-              id="username"
-              name="name"
-              value={name}
-              placeholder="Name"
-              onChange={handleChange}
-            />
-          </div>
-          <div className="input-group">
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={email}
-              placeholder="E-mail"
-              onChange={handleChange}
-            />
-          </div>
-          <div className="input-group">
-            <textarea
-              id="message"
-              name="message"
-              value={message}
-              placeholder="Your Message"
-              onChange={handleChange}
-            ></textarea>
-          </div>
-          <button type="submit" className="btn-yellow">Send Message <i className="fa-regular fa-arrow-up-right"></i></button>
-        </form>
+        {messageSent ? (
+          <div className="success-message">Message has been sent, thank you for your information!</div>
+        ) : (
+          <form onSubmit={handleSubmit}>
+            <h1>Leave us a message<br /> for any information.</h1>
+            <div className="input-group">
+              <input
+                type="text"
+                name="name"
+                value={name}
+                placeholder="Name"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="input-group">
+              <input
+                type="email"
+                name="email"
+                value={email}
+                placeholder="E-mail"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="input-group">
+              <textarea
+                name="message"
+                value={message}
+                placeholder="Your Message"
+                onChange={handleChange}
+              ></textarea>
+            </div>
+            <button type="submit" className="btn-yellow">Send Message <i className="fa-regular fa-arrow-up-right"></i></button>
+          </form>
+        )}
       </div>
     </div>
   )
