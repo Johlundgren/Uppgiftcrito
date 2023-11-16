1d449286a5d5afbd06fd87c613e95991fc3a8a14
Johan <gunbald@live.se>
2023-11-16 12:37:10 +0100

    Vet inte varför det fungerar men det fungerar nu

--- src/components/Companyinformation.jsx
+++ src/components/Companyinformation.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@styles/_Companyinformation.scss'
 import CompanyInfoBox from './CompanyInfoBox'
+import { Link } from 'react-router-dom'; 
 
 const Companyinformation = () => {
   return (
